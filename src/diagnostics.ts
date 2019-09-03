0883473d3d21c41ef8e34ccaf5483139027b9c79
Janne <janne.a.varjo@gmail.com>
2019-09-03 20:18:33 +0300

    diagnostic improvements, config support

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
-import {localize} from './extension';
 import { isInteger } from './validation';
+import { hrtimeToSeconds, getRelativeFileName, combineDiagnostics } from './util';
+import Config from './configuration';
 
 /**
  * Provides diagnostics for a .ucs file.
@@ -9,197 +10,217 @@ export default class UCSDiagnosticsProvider implements vscode.Disposable {
     private readonly diagnosticCollection: vscode.DiagnosticCollection;
     private readonly disposables: vscode.Disposable[] = [];
     private isProgressNotificationVisible = false;
+    private config: Config;
+    private processDocumentTimeout: NodeJS.Timeout | null = null;
 
-    constructor() {
+    constructor(config: Config) {
+        this.config = config;
+
         this.diagnosticCollection = vscode.languages.createDiagnosticCollection('ucs');
 
         // Listen for events that should trigger document validation
         this.disposables.push(
             vscode.workspace.onDidOpenTextDocument(this.onOpenTextDocument),
-            // TODO this event seems to fire twice on first text editor edit
             vscode.workspace.onDidChangeTextDocument(this.onDocumentTextChanged),
+            this.config.onDidChange(this.onConfigChanged),
         );
-
-        // Process documents that are already open
-        vscode.workspace.textDocuments.forEach(document => this.processDocument(document, 'initial'));
+           
+        this.processAllWorkspaceDocuments();
     }
 
-    private onOpenTextDocument = (e: vscode.TextDocument) => this.processDocument(e, 'onDidOpenTextDocument');
-    private onDocumentTextChanged = (e: vscode.TextDocumentChangeEvent) => this.processDocument(e.document, 'onDidChangeTextDocument');
+    private onOpenTextDocument = (e: vscode.TextDocument) => this.processDocument(e);
+    private onDocumentTextChanged = (e: vscode.TextDocumentChangeEvent) => this.processDocument(e.document);
+    private onConfigChanged = () => this.processAllWorkspaceDocuments();
 
-    private processDocument(document: vscode.TextDocument, eventName: string) {
+    private processAllWorkspaceDocuments() {
+        vscode.workspace.textDocuments.forEach(document => this.processDocument(document));
+    }
+
+    private processDocument(document: vscode.TextDocument) {
         if (document.languageId !== 'ucs') {
             return;
         }
 
-        console.log(`processDocument: ${eventName}`);
-
-        // Open, more than thousand lines: Show progress message
-        if (!document.isClosed && document.lineCount > 1000 && this.isProgressNotificationVisible == false) {
-            // Resolve a friendly file name
-            let fileName = document.fileName;
-            for (const folder of vscode.workspace.workspaceFolders || []) {
-                if (document.uri.path.startsWith(folder.uri.path)) {
-                    fileName = document.uri.path.substring(folder.uri.path.length);
-                    if (fileName.startsWith('/')) {
-                        fileName = fileName.substring(1);
-                    }
-                    break;
-                }
-            }
-
-            vscode.window.withProgress({
-                location: vscode.ProgressLocation.Notification,
-                cancellable: false,
-                title: localize('notifications.processingLargeFile', '[Large file] Processing {file}...').replace(
-                    '{file}', fileName,
-                ),
-
-            }, () => new Promise((resolve, reject) => {
-                this.isProgressNotificationVisible = true;
-
-                setTimeout(() => {
-                    this.diagnoseDocument(document);
-                    resolve();
-                    this.isProgressNotificationVisible = false;
-                }, 100);
-            }));
-        } else {
-            this.diagnoseDocument(document);
+        if (this.processDocumentTimeout != null) {
+            clearTimeout(this.processDocumentTimeout);
         }
+
+        this.processDocumentTimeout = setTimeout(() => {
+            this.processDocumentTimeout = null;
+
+            // Open, more than thousand lines: Show progress message
+            if (!document.isClosed && document.lineCount > 1000 && this.isProgressNotificationVisible == false) {
+                // Resolve a friendly file name
+                let fileName = getRelativeFileName(document);
+    
+                vscode.window.withProgress({
+                    location: vscode.ProgressLocation.Notification,
+                    cancellable: false,
+                    title: `[Large file] Processing ${fileName}...`
+                }, () => new Promise((resolve, reject) => {
+                    this.isProgressNotificationVisible = true;
+    
+                    setTimeout(() => {
+                        this.diagnoseDocument(document);
+                        resolve();
+                        this.isProgressNotificationVisible = false;
+                    }, 100);
+                }));
+            } else {
+                this.diagnoseDocument(document);
+            }
+        }, 100);
     }
 
     private diagnoseDocument(document: vscode.TextDocument) {
         const diagnostics: vscode.Diagnostic[] = [];
         const text = document.getText();
-        const lines = text.split('\n');
+        const lines = text.split(/\r?\n/);
         const definedLocStringIds: {[key: string]: vscode.Range} = {};
         const definedLocStringMessages: {[key: string]: vscode.Range} = {};
 
+        const duplicateIds: Record<string, vscode.DiagnosticRelatedInformation[]> = {};
+        const duplicateMessages: Record<string, vscode.DiagnosticRelatedInformation[]> = {};
+        const source = `UCS`;
+
         for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
             const line = lines[lineIndex];
-           
+
             // Check for empty lines. Allow last line of file to be empty.
-            if (line.length == 0) {
+            if (!this.config.ignoreEmptyLines && line.length == 0) {
                 if (lineIndex != lines.length - 1) {
                     diagnostics.push({
                         range: new vscode.Range(
                             new vscode.Position(lineIndex, 0),
                             new vscode.Position(lineIndex, line.length),
                         ),
-                        message: localize('diagnostics.warning.emptyLine', 'Empty line.'),
-                        
+                        message: 'Empty line.',
                         severity: vscode.DiagnosticSeverity.Warning,
+                        source,
                     });
                 }
                 continue;
             }
             
-            let tabIndex = line.indexOf('\t');
+            const tabIndex = line.indexOf('\t');
             
             // Check for lines that don't contain at least one tab character
-            if (tabIndex == -1) {
+            if (line.length > 0 && tabIndex == -1) {
                 diagnostics.push({
                     range: new vscode.Range(
                         new vscode.Position(lineIndex, 0),
                         new vscode.Position(lineIndex, line.length),
                     ),
-                    message: localize('diagnostics.error.invalidLocString', 'Invalid LOCString: Missing tab character (required between ID number and the message)'),
-                    
+                    message: 'Invalid LOCString: Missing tab character (required between ID number and the message)',
                     severity: vscode.DiagnosticSeverity.Error,
+                    source,
                 });
                 continue;
             }
 
             const id = line.substring(0, tabIndex);
             const message = line.substring(tabIndex + 1);
-            const normalizedMessage = message.trim().toLowerCase();
+            const normalizedMessage = message.trim();
             
             // Check for empty ID numbers
-            if (id.length == 0) {
+            if (line.length > 0 && id.length == 0) {
                 diagnostics.push({
                     range: new vscode.Range(
                         new vscode.Position(lineIndex, 0),
                         new vscode.Position(lineIndex, tabIndex),
                     ),
-                    message: localize('diagnostics.error.missingLocStringId', 'Invalid LOCString: Missing ID number.'),
-                    
+                    message: 'Invalid LOCString: Missing ID number.',
                     severity: vscode.DiagnosticSeverity.Error,
+                    source,
                 });
                 continue;
             }
             
             // Check for non-numeric ID numbers
-            if (!isInteger(id)) {
+            if (line.length > 0 && !isInteger(id)) {
                 diagnostics.push({
                     range: new vscode.Range(
                         new vscode.Position(lineIndex, 0),
                         new vscode.Position(lineIndex, tabIndex),
                     ),
-                    message: localize('diagnostics.error.invalidLocStringId', 'Invalid LOCString: Invalid ID number (must be an integer).'),
-                    
+                    message: 'Invalid LOCString: Invalid ID number (must be an integer).',
                     severity: vscode.DiagnosticSeverity.Error,
+                    source,
                 });
                 continue;
             }
 
             // Check for empty messages
-            if (message.trim().length == 0) {
+            if (!this.config.ignoreEmptyMessages && line.length > 0 && message.length == 0) {
                 diagnostics.push({
                     range: new vscode.Range(
                         new vscode.Position(lineIndex, 0),
                         new vscode.Position(lineIndex, line.length),
                     ),
-                    message: localize('diagnostics.warning.emptyMessage', 'Empty message.'),
-                    
+                    message: 'Empty message.',
                     severity: vscode.DiagnosticSeverity.Warning,
+                    source,
                 });
                 continue;
             }
 
             // Check for duplicate ID numbers
-            if (definedLocStringIds[id] != null) {
-                diagnostics.push({
-                    range: new vscode.Range(
-                        new vscode.Position(lineIndex, 0),
-                        new vscode.Position(lineIndex, tabIndex),
-                    ),
-                    message: localize('diagnostics.error.duplicateLocStringId', 'Duplicate ID (first occurrance on line {line}).')
-                        .replace('{line}', `${lineIndex}`)
-                    ,
-                    
-                    severity: vscode.DiagnosticSeverity.Error,
+            if (id.length > 0 && definedLocStringIds[id] != null) {
+                if (duplicateIds[id] == null) duplicateIds[id] = [];
+
+                duplicateIds[id].push({
+                    location: {
+                        uri: document.uri,
+                        range: new vscode.Range(
+                            new vscode.Position(lineIndex, 0),
+                            new vscode.Position(lineIndex, tabIndex),
+                        ),
+                    },
+                    message: 'Duplicate ID.',
                 });
             }
 
             // Check for duplicate messages
-            if (definedLocStringMessages[normalizedMessage] != null) {
-                diagnostics.push({
-                    range: new vscode.Range(
-                        new vscode.Position(lineIndex, tabIndex + 1),
-                        new vscode.Position(lineIndex, line.length),
-                    ),
-                    message: localize('diagnostics.warning.duplicateLocStringMessage', 'Duplicate message (first occurrance on line {line}).')
-                        .replace('{line}', `${lineIndex}`)
-                    ,
-                    
-                    severity: vscode.DiagnosticSeverity.Warning,
+            if (normalizedMessage.length > 0 && !this.config.ignoreDuplicateMessages && definedLocStringMessages[normalizedMessage] != null) {
+                if (duplicateMessages[normalizedMessage] == null) duplicateMessages[normalizedMessage] = [];
+
+                duplicateMessages[normalizedMessage].push({
+                    location: {
+                        uri: document.uri,
+                        range: new vscode.Range(
+                            new vscode.Position(lineIndex, tabIndex + 1),
+                            new vscode.Position(lineIndex, line.length),
+                        )
+                    },
+                    message: `Duplicate message.`,
                 });
             }
-
-            definedLocStringIds[id] = new vscode.Range(
-                new vscode.Position(lineIndex, 0),
-                new vscode.Position(lineIndex, tabIndex)
-            );
-
-            definedLocStringMessages[normalizedMessage] = new vscode.Range(
-                new vscode.Position(lineIndex, tabIndex),
-                new vscode.Position(lineIndex, line.length)
-            );
+            
+            if (id.length > 0) {
+                definedLocStringIds[id] = new vscode.Range(
+                    new vscode.Position(lineIndex, 0),
+                    new vscode.Position(lineIndex, tabIndex)
+                );
+            }
+            
+            if (normalizedMessage.length > 0) {
+                definedLocStringMessages[normalizedMessage] = new vscode.Range(
+                    new vscode.Position(lineIndex, tabIndex),
+                    new vscode.Position(lineIndex, line.length)
+                );
+            }
         }
 
+        for (const key in duplicateIds) {
+            diagnostics.push(combineDiagnostics(duplicateIds[key], {source, severity: vscode.DiagnosticSeverity.Error}));
+        }
+
+        for (const key in duplicateMessages) {
+            diagnostics.push(combineDiagnostics(duplicateMessages[key], {source}));
+        }
         this.diagnosticCollection.set(document.uri, diagnostics);
     }
+
 
     public dispose() {
         this.disposables.forEach(item => item.dispose());
--- src/validation.ts
+++ src/validation.ts
@@ -1,7 +1,4 @@
 export function isInteger(value: string) {
-    // Zero prefixes not allowed (just a zero is fine though)
-    if (value.length > 1 && value[0] == '0') {
-        return false;
-    }
-    return value.split('').every(char => (char >= '0' && char <= '9'));
+    const number = Math.floor(Number(value));
+    return number !== Infinity && String(number) === value && number >= 0;
 }
