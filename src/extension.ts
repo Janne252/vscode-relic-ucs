e0affed9dc001266502b4bb22ee7de5bbcc671e3
Janne <janne.a.varjo@gmail.com>
2019-09-06 17:48:20 +0300

    Remove formatter in favor of default language settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,11 @@
 import * as vscode from 'vscode';
 import UCSDiagnosticsProvider from './diagnostics';
-import UCSTabFormatter from './formatting';
-import Config from './configuration';
+import UCSConfig from './configuration';
 
 export async function activate(context: vscode.ExtensionContext) {
-	const config = new Config();
+	const config = new UCSConfig();
 	
 	context.subscriptions.push(new UCSDiagnosticsProvider(config));
-	context.subscriptions.push(new UCSTabFormatter(config));
 	context.subscriptions.push(config);
 }
 
